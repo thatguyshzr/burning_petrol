71f7a774d6f21dd65428821a195e39116a26913b
Shahzar Ansari <shahzar.ansari94@gmail.com>
2021-09-08 12:13:07 +0530

    Add files via upload

--- mil_calc.js
+++ mil_calc.js
@@ -3,19 +3,14 @@ function km_petrol() {
 	mileage = document.getElementById("mileage").value
 	petrol_used = (km/mileage).toFixed(2)
 	console.log("Petrol used: "+petrol_used + 'L')
+	document.getElementById('petr').innerHTML.value = petrol_used;
 	// current_petrol_price= 107.0 // scrap in real-time
-	current_petrol_price= get_petrol_price()
-	money = petrol_used * current_petrol_price
-	console.log("Rs " + money)
-	console.log(current_petrol_price)
+	// current_petrol_price= get_petrol_price()
+	// money = petrol_used * current_petrol_price
+	// console.log("Rs " + money)
+	// console.log(current_petrol_price)
 }
 
-const request = require('request')
-request('https://www.reddit.com/r/programming.json', function (
-  error,
-  response,
-  body
-) {
-  console.error('error:', error)
-  console.log('body:', body)
-})
+function get_petrol_price() {
+	return 107
+}
